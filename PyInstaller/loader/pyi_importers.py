0d1d7da417681ba1060cccf88fb46293cb8fb759
AlexeySkvortsov <alexeyn.skvortsov@gmail.com>
2015-08-10 21:06:02 +0600

    Failed to start compiled exe from non-ascii path on external usb-flash drive - partially fixed to work on linux (closed #1396)

--- PyInstaller/loader/pyi_importers.py
+++ PyInstaller/loader/pyi_importers.py
@@ -133,7 +133,12 @@ class FrozenImporter(object):
         for pyz_filepath in sys.path:
             try:
                 # Unzip zip archive bundled with the executable.
-                self._pyz_archive = ZlibArchive(unicode(pyz_filepath, "utf-8"))
+                if sys.getfilesystemencoding() == 'mbcs':
+                    # On Windows NT+, file names are Unicode natively
+                    pyz_filepath_encoded = unicode(pyz_filepath, 'utf-8')
+                else:
+                    pyz_filepath_encoded = pyz_filepath
+                self._pyz_archive = ZlibArchive(pyz_filepath_encoded)
                 # Verify the integrity of the zip archive with Python modules.
                 self._pyz_archive.checkmagic()
                 # End this method since no Exception was raised we can assume
